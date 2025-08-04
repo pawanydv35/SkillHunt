c451f2937ec5c9480d2148945360823c5f22934a
Pawan Yadav <pawanydv3010@gmail.com>
2025-08-04 14:59:17 +0530

    navbar

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,12 @@ import Link from "next/link";
 export default function navbar() {
   return (
     <div>
-       
+       <li>
+        <ul>Find Jobs</ul>
+        <ul>Messages</ul>
+        <ul>Hiring</ul>
+        <ul>FAQs</ul>
+       </li>
        
       
     </div>
